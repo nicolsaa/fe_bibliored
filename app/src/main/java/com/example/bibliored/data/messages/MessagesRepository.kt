f5e60b3b529e0eeba34bc7b09707bee03578fb69
nicol saavedra <n.saavedrag94@gmail.com>
2025-11-27 21:04:36 -0300

    elimine el mock de las conversaciones

--- app/src/main/java/com/example/bibliored/data/messages/MessagesRepository.kt
+++ app/src/main/java/com/example/bibliored/data/messages/MessagesRepository.kt
@@ -5,20 +5,9 @@ import com.example.bibliored.model.messages.Message
 
 class MessagesRepository {
 
-    private val conversations = mutableListOf<Conversation>(
-        Conversation("1", listOf("Usuario1", "Usuario2"), participantIds = listOf("id1", "id2"), lastMessage = "Hola! ¿Cómo estás?", timestamp = System.currentTimeMillis() - 10000, unreadCount = 1),
-        Conversation("2", listOf("Usuario1", "Usuario3"), participantIds = listOf("id1", "id3"), lastMessage = "Nos vemos mañana.", timestamp = System.currentTimeMillis() - 8000, unreadCount = 0)
-    )
+    private val conversations = mutableListOf<Conversation>()
 
-    private val messages = mutableMapOf<String, MutableList<Message>>(
-        "1" to mutableListOf(
-            Message("m1", "1", "Usuario2", "id2", "Hola! ¿Cómo estás?", System.currentTimeMillis() - 10000),
-            Message("m2", "1", "Usuario1", "id1", "Todo bien, ¿y tú?", System.currentTimeMillis() - 5000)
-        ),
-        "2" to mutableListOf(
-            Message("m3", "2", "Usuario3", "id3", "Nos vemos mañana.", System.currentTimeMillis() - 8000)
-        )
-    )
+    private val messages = mutableMapOf<String, MutableList<Message>>()
 
     fun getConversations(): List<Conversation> {
         return conversations
@@ -36,8 +25,6 @@ class MessagesRepository {
     fun addConversation(conversation: Conversation) {
         conversations.add(0, conversation)
     }
-
-
 
     fun deleteConversation(conversationId: String) {
         conversations.removeAll { it.id == conversationId }
